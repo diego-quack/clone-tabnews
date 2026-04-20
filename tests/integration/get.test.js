42855937a7e58aeb95bfc03c441b93ed2221fa07
Diego Adami Biancardi <88746361+diego-quack@users.noreply.github.com>
2026-04-20 18:09:39 +0000

    torna o database.js mais robusto contra erros

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -7,9 +7,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.version).toEqual("16.13");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.opened_connections).toBe(2);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 
   console.log(responseBody);
 });
